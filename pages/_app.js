88b401a218e297bd304481ec56659e4d2a27cfe9
SolomakhaAndrii <asolomaha@ukr.net>
2026-04-19 21:38:10 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-G2F6MRS6M4"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G2F6MRS6M4');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
